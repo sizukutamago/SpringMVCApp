2aa8323a2e51df701cbb77f6fbfd6a4dd3c54330
sizukutamago <tanaka@tam-bourine.co.jp>
2019-10-31 20:54:15 +0900

    controller

--- src/main/java/com/sample/mvc/HelloController.java
+++ src/main/java/com/sample/mvc/HelloController.java
@@ -4,14 +4,86 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class HelloController {
-    @RequestMapping("/hello/{num}")
-    public String index(@PathVariable int num) {
-        int total = 0;
-        for (int i = 1; i <= num; i++) {
-            total += i;
-        }
-        return "you send :" + num + ".\n" + "total: " + total;
+    private int id;
+
+    private List<String> names = new ArrayList<>();
+    private List<String> mails = new ArrayList<>();
+
+    public HelloController() {
+        super();
+        names.add("taro");
+        names.add("hanako");
+        names.add("sachiko");
+        names.add("tuyano");
+        names.add("mami");
+        mails.add("taro@yamada");
+        mails.add("hanako@flower");
+        mails.add("sachiko@happy");
+        mails.add("shoda@tuyano.com");
+        mails.add("mami@mumemo");
+    }
+
+    @RequestMapping("/hello")
+    public XmlData index() {
+        XmlData obj = new XmlData(id, names.get(id), mails.get(id));
+        id = ++id == names.size() ? 0 : id;
+        return obj;
+    }
+}
+
+@XmlRootElement(name = "xmldata")
+@XmlAccessorType(XmlAccessType.NONE)
+class XmlData {
+    @XmlAttribute
+    private Integer id;
+
+    @XmlElement
+    private String name;
+
+    @XmlElement
+    private String email;
+
+    public XmlData() {
+        super();
+        this.id = 0;
+        this.name = "noname";
+        this.email = "no@mail";
+    }
+
+    public XmlData(Integer id, String name, String email) {
+        this();
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
